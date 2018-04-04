d1531f48f6a8df79a50b9f4fa73e479759fdbb12
Emily Tran <trane@oxy.edu>
2018-04-03 22:01:58 -0700

    Added horizontal lines

--- play/mini/mini.js
+++ play/mini/mini.js
@@ -5,13 +5,14 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - 50;
 var ctx = canvas.getContext("2d"); // 2D rendering space
 
-var PARKING_SPACING = 150; // spacing between each vertical parking line
+var PARKING_SPACING = 120; // spacing between each vertical parking line
 var VERTICAL_LINE_LENGTH = 200;
-var NUM_SPACES = 5;
+var PARKING_START_LOCATION = 10;
+var NUM_SPACES = 4;
+var SCALE_FACTOR = 0.2;
 
-
+// LOAD CAR IMAGE
 var img = new Image();
-var SCALE_FACTOR = 0.3;
 img.onload = function(){
   ctx.drawImage(img, window.innerWidth/2, window.innerHeight/2, img.width*SCALE_FACTOR, img.height*SCALE_FACTOR);
 }
@@ -19,20 +20,35 @@ img.onload = function(){
 img.src = "../img/player_car.png";
 
 
+
+drawVerticalLines();
+drawHorizontalLines();
+
+// RESET FUNCTION
 $("#reset").click(function(){
   ctx.clearRect(0,0,canvasWidth,canvasHeight);
-  drawVerticalLines(NUM_SPACES);
+  drawVerticalLines();
+  drawHorizontalLines();
   ctx.drawImage(img, window.innerWidth/2, window.innerHeight/2, img.width*SCALE_FACTOR, img.height*SCALE_FACTOR);
 })
-drawVerticalLines(NUM_SPACES);
+
+// DRAW HORIZONTAL LINES
+function drawHorizontalLines(){
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.moveTo(PARKING_SPACING, PARKING_START_LOCATION);
+  ctx.lineTo(PARKING_SPACING*(NUM_SPACES+1), PARKING_START_LOCATION);
+  ctx.stroke();
+}
+
 // DRAW VERTICAL LINES
-function drawVerticalLines(NUM_SPACES){
+function drawVerticalLines(){
   var verticalLines = [];
-  var yPos = 10;
+  var yPos = PARKING_START_LOCATION;
   var xPos;
 
-  // create vertical line data
-  for(var i = 1; i < NUM_SPACES; i++){
+  for(var i = 1; i < NUM_SPACES+2; i++){
     ctx.lineWidth = 10;
     xPos = i * PARKING_SPACING;
     ctx.beginPath();
@@ -78,7 +94,8 @@ var canvasOffset=$("#canvas").offset();
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){
           ctx.clearRect(0,0,canvasWidth,canvasHeight);
-          drawVerticalLines(NUM_SPACES);
+          drawVerticalLines();
+          drawHorizontalLines();
           ctx.drawImage(img,canMouseX-128/2,canMouseY-120/2, img.width*SCALE_FACTOR, img.height*SCALE_FACTOR);
       }
     }
